07c0c29ea84247f4fddc3305518357faecd345d9
dothework <marktc@gmail.com>
2017-11-13 12:56:46 -0500

    checkpoint five work, added assets too

--- src/components/album.js
+++ src/components/album.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-  <section class="album">
-    Album will go here
-  </section>
-)
+class Album extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.match.params.slug } Album will go here 
+      </div>
+    );
+  }
+}
 
 export default Album;
--- src/components/library.js
+++ src/components/library.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
-const Library = () => (
-  <section class="library">
-    Library will go here
-  </section>
-)
+class Library extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { albums: albumData };
+  }
+
+  render() {
+   return (
+     <section class='library'>
+     {
+       this.state.albums.map(album =>
+         <Link to={`/album/${album.slug}`}>
+         <img src={ album.albumCover } />
+         <div>{ album.title }</div>
+         <div>{ album.artist }</div>
+         <div>{ album.songs.length } songs</div>
+         </Link>
+       )
+     }
+     </section>
+    )
+  }
+}
 
 export default Library;
